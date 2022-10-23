a1057fe434186038b5300f42dafaa0a49ae2c00f
davidtarrab <dudutarrab@gmail.com>
2022-10-23 05:22:22 -0300

    first commit

---
+++ src/screens/new-place/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { View, Text, ScrollView, TextInput, Button } from "react-native";
+import { useDispatch } from "react-redux";
+import ImageSelector from "../../components/image-selector";
+
+import { addPlace } from "../../store/place.slice";
+import colors from "../../utils/colors";
+import { styles } from "./styles";
+
+const NewPlace = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+
+  const onHandleChange = (text) => {
+    setTitle(text);
+  };
+
+  const onHandleSubmit = () => {
+    dispatch(addPlace(title));
+    navigation.navigate("Places");
+  };
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Titulo</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nuevo ubicacion"
+          onChangeText={onHandleChange}
+          value={title}
+        />
+        <ImageSelector onImage={()=>null} />
+        <Button title="Grabar dirección" color={colors.primary} onPress={onHandleSubmit} />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default NewPlace;
