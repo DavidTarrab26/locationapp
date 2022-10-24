d3a2d000f1db57b7c68bcc54d1e53791e273f244
davidtarrab <dudutarrab@gmail.com>
2022-10-24 01:18:54 -0300

    system move

--- src/store/place.slice.js
+++ src/store/place.slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import * as FileSystem from "expo-file-system";
 import Place from "../model/place"
 
 const initialState = {
@@ -10,11 +11,26 @@ const placeSlice = createSlice({
   initialState,
   reducers: {
     addPlace: (state, action) => {
-      const newPlace = new Place(Date.now(), action.payload);
+      const newPlace = new Place(Date.now(), action.payload.title, action.payload.image);
       state.places.push(newPlace);
     },
   },
 });
 export const {addPlace} = placeSlice.actions;
-
+export const savePlace = (title, image) => {
+  return async (dispatch) => {
+    /* const fileName = image.split("/").pop();
+    const Path = FileSystem.documentDirectory + fileName; */
+    try {
+      /* await FileSystem.moveAsync({
+        from: image,
+        to: Path,
+      }); */
+      dispatch(addPlace({ title, image}));
+    } catch (error) {
+      console.warn("error", error);
+      throw error;
+    }
+  };
+};
 export default placeSlice.reducer;
